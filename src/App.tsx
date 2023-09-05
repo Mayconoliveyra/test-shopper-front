b1a2591a00bf04d26f5999da288b284a2ba424a4
Maycon Oliveira <mayconbrito1998@hotmail.com>
2023-09-05 15:10:55 -0300

    refactor: Adicionado 'CssBaseline'

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { CssBaseline } from "@mui/material";
 
 export const App = () => {
   return (
     <BrowserRouter>
+      {/* Remove os estilos que vem por padrão no html/body. */}
+      <CssBaseline />
       <AppRoutes />
     </BrowserRouter>
   );
