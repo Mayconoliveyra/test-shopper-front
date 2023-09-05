9a1341127723b80d4500803a2bea034c91b56dd7
Maycon Oliveira <mayconbrito1998@hotmail.com>
2023-09-05 16:11:28 -0300

    feat: Criado página de formação de preço

---
+++ src/pages/Pricing/index.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import { LayoutBasel } from "../../shared/layouts/LayoutBase";
+
+export const Pricing = () => {
+  return (
+    <LayoutBasel>
+      <Typography>Olá, mundo!.</Typography>
+    </LayoutBasel>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import { Pricing } from "../pages/Pricing";
+
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route
-        path="/formacao-precos"
-        element={<p>Página Formação de Preços.</p>}
-      />
+      <Route path="/" element={<Pricing />} />
 
       <Route path="*" element={<Navigate to="/formacao-precos" />} />
     </Routes>
